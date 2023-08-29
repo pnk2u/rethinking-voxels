8c3f28d005176a78001858aa61db05106bc0feca
gri573 <david.dg.grimm@gmail.com>
2023-08-29 18:46:52 +0200

    move options to common.glsl

--- shaders/lib/common.glsl
+++ shaders/lib/common.glsl
@@ -26,6 +26,9 @@
         #define POM
     #endif
 
+    const int voxelDetailAmount = 4; //[0 1 2 3 4 5 6]
+    #define VX_VOL_SIZE 0 //[0 1 2 3]
+
     #define REALTIME_SHADOWS
     #define SHADOW_QUALITY 2 //[1 2 3 4 5]
     const float shadowDistance = 192.0; //[64.0 80.0 96.0 112.0 128.0 160.0 192.0 224.0 256.0 320.0 384.0 512.0 768.0 1024.0]
--- shaders/lib/vx/SSBOs.glsl
+++ shaders/lib/vx/SSBOs.glsl
@@ -6,9 +6,15 @@
 	#endif
 
 	// constants
-	const ivec3 voxelVolumeSize = ivec3(256, 128, 256);
-	const int voxelDetailAmount = 4;
-
+	#if VX_VOL_SIZE == 0
+		const ivec3 voxelVolumeSize = ivec3(96, 64, 96);
+	#elif VX_VOL_SIZE == 1
+		const ivec3 voxelVolumeSize = ivec3(128, 96, 128);
+	#elif VX_VOL_SIZE == 2
+		const ivec3 voxelVolumeSize = ivec3(256, 128, 256);
+	#elif VX_VOL_SIZE == 3
+		const ivec3 voxelVolumeSize = ivec3(512, 128, 512);
+	#endif
 	// voxelisation-related mapping functions
 	#include "/lib/vx/mapping.glsl"
 
