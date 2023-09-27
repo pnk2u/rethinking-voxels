840204d0f5a6c08656cb673da736f5d969989117
gri573 <david.dg.grimm@gmail.com>
2023-09-27 17:53:03 +0200

    fully initialize blockIdMap

--- shaders/program/setup1.glsl
+++ shaders/program/setup1.glsl
@@ -1,6 +1,6 @@
 #include "/lib/common.glsl"
 
-const ivec3 workGroups = ivec3(511, 1, 1);
+const ivec3 workGroups = ivec3(512, 1, 1);
 layout(local_size_x=32, local_size_y = 1, local_size_z = 1) in;
 
 uniform sampler2D colortex3;
@@ -11,6 +11,13 @@ uniform sampler2D colortex3;
 
 void main() {
     int mat = int(gl_GlobalInvocationID.x);
-    ivec2 mappedMat0 = ivec2(texelFetch(colortex3, ivec2(mat % 512, mat / 512), 0).rg * 255 + 0.5);
-    blockIdMap[mat] = mappedMat0.x + (mappedMat0.y << 8);
+    if (mat / 512 < textureSize(colortex3, 0).y) {
+        ivec2 mappedMat0 = ivec2(texelFetch(colortex3, ivec2(mat % 512, mat / 512), 0).rg * 255 + 0.5);
+        blockIdMap[mat] = mappedMat0.x + (mappedMat0.y << 8);
+    } else {
+        blockIdMap[mat] = 0;
+    }
+    for (int k = 0; k < 7; k++) {
+        blockIdMap[16384 + 7 * mat + k] = 0;
+    }
 }
